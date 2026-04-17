735efa06572dc85a40b44f5c00a2892b80e5cb9f
Ting Lu <tinglu30@gmail.com>
2026-04-16 21:47:19 -0400

    updated resume, added GA

--- src/data/work-education.ts
+++ src/data/work-education.ts
@@ -1,71 +1,82 @@
-export const work = [{
+export const work = [
+  {
     name: "Mixel Studio",
     href: "https://mixelstudio.com/",
     title: "Software Engineer",
     logo: "/mixel.jpg",
     start: "June 2024",
-    description: ["Implemented a web-based XR slide editor that enables users to create and collaborate on immersive presentations using Meta’s MRUK for mixed reality and WebRTC for real-time communication.",
-        "Deployed a Spring Boot backend on AWS using Docker containers and implemented a robust CI/CD, enabling automated build, test, and deployment workflows for seamless, scalable, and reliable application delivery",
+    description: [
+      "Implemented a web-based XR slide editor that enables users to create and collaborate on immersive presentations using Meta’s MRUK for mixed reality and WebRTC for real-time communication.",
+      "Deployed a Spring Boot backend on AWS using Docker containers and implemented a robust CI/CD, enabling automated build, test, and deployment workflows for seamless, scalable, and reliable application delivery",
     ],
-},
-{
+  },
+  {
     name: "Forgotten Felines",
     href: "https://www.forgottenfelines.com/",
     title: "Full Stack Developer",
     logo: "/cat.png",
     start: "October 2024",
     end: "February 2025",
-    description: ["Built and launched a Next.js web application with AWS integration to streamline cat rescue operations, improving tracking efficiency by 40% and focusing on low latency and high availability for operational data."],
-},
-{
+    description: [
+      "Built and launched a Next.js web application with AWS integration to streamline cat rescue operations, improving tracking efficiency by 40% and focusing on low latency and high availability for operational data.",
+    ],
+  },
+  {
     name: "Linens N Love",
     href: "https://linensnlove.org/",
     title: "Front End Developer",
     logo: "/lnl.jpg",
     start: "May 2024",
     end: "September 2024",
-    description: ["Developed a React web application with Express.js and MongoDB as the backend to streamline linen donations between hotels, shelters, and volunteers, increasing efficiency by 25% and focusing on scalable solutions for donation logistics"],
-}
-
-]
-
+    description: [
+      "Developed a React web application with Express.js and MongoDB as the backend to streamline linen donations between hotels, shelters, and volunteers, increasing efficiency by 25% and focusing on scalable solutions for donation logistics",
+    ],
+  },
+];
 
 export const education = [
-    {
-        name: "University of Michigan",
-        href: "https://www.umich.edu",
-        title: "Bachelor of Science in Computer Science",
-        logo: "/umich.png",
-        start: "2022",
-        end: "2025",
-    },
-    {
-        name: "Bergen Community College",
-        href: "https://bergen.edu/",
-        title: "Associate of Science in Computer Science",
-        logo: "/bcc.png",
-        start: "2020",
-        end: "2022",
-    },
-]
+  {
+    name: "Georgia Institute of Technology",
+    href: "https://www.gatech.edu",
+    title: "Masters of Science in Computer Science",
+    logo: "/gatech.png",
+    start: "2026",
+    end: "2028",
+  },
+  {
+    name: "University of Michigan",
+    href: "https://www.umich.edu",
+    title: "Bachelor of Science in Computer Science",
+    logo: "/umich.png",
+    start: "2022",
+    end: "2025",
+  },
+  {
+    name: "Bergen Community College",
+    href: "https://bergen.edu/",
+    title: "Associate of Science in Computer Science",
+    logo: "/bcc.png",
+    start: "2020",
+    end: "2022",
+  },
+];
 
 export const skills = [
-    "JavaScript",
-    "TypeScript",
-    "Python",
-    "Java",
-    "C#",
-    "C++",
-    "React",
-    "Next.js",
-    "Tailwind CSS",
-    "Node.js",
-    "Express",
-    "MongoDB",
-    "PostgreSQL",
-    "Git",
-    "GitHub",
-    "Docker",
-    "AWS",
-]
-
+  "JavaScript",
+  "TypeScript",
+  "Python",
+  "Java",
+  "C#",
+  "C++",
+  "React",
+  "Next.js",
+  "Tailwind CSS",
+  "Node.js",
+  "Express",
+  "MongoDB",
+  "PostgreSQL",
+  "Git",
+  "GitHub",
+  "Docker",
+  "AWS",
+];
